e79f985dc91c2f5cf18df3bd86484cd3c5d590dc
efields@mcitpnetworks.com <efields@mcitpnetworks.com>
2020-12-13 21:13:16 -0500

    Add Secrets

--- main.tf
+++ main.tf
@@ -1,5 +1,9 @@
 provider "azurerm" {
   version = "=2.0.0"
+  subscription_id = var.sub
+  client_id       = "${var.client_id}"
+  client_secret   = "${var.client_secret}"
+  tenant_id       = "${var.tenant_id}"
   features {}
 }
 
