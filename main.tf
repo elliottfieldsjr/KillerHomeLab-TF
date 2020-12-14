b97e96d557d48c327542326014b6609d5deb4054
efields@mcitpnetworks.com <efields@mcitpnetworks.com>
2020-12-13 22:16:04 -0500

    .

--- main.tf
+++ main.tf
@@ -8,11 +8,6 @@ terraform {
 }
 
 provider "azurerm" {
-  version = "=2.0.0"
-  subscription_id = var.sub
-  client_id       = "${var.client_id}"
-  client_secret   = "${var.client_secret}"
-  tenant_id       = "${var.tenant_id}"
   features {}
 }
 
